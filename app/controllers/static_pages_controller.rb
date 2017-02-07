7674130f6a4c4a1e0d8fd988fc4acf4aba1666a0
Hiroki Yano <sxcxyxh@gmail.com>
2017-02-07 13:45:05 +0900

    ok

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+  @users = User.all
+  @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+    marker.lat user.latitude
+    marker.lng user.longitude
+    marker.infowindow user.description
+    marker.json({title: user.title})
   end
+end
 
   def help
   end
