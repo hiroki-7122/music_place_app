4ebd09a4c68c7aab39639a3e0ab2abde1b788044
Hiroki Yano <sxcxyxh@gmail.com>
2017-02-08 07:29:35 +0900

    search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -18,14 +18,18 @@ class StaticPagesController < ApplicationController
   def help
   end
 
+  def search_form
+  end
+
   def search
+    @key=params[:q]
     # hash形式でパラメタ文字列を指定し、URL形式にエンコード
     params = URI.encode_www_form({
       lang: 'ja_jp',
       entry: 'music',
       media: 'music',
       country: 'JP',
-      term: "UVERworld",
+      term: "#{@key}",
       limit: 10,
     })
     # URIを解析し、hostやportをバラバラに取得できるようにする
@@ -43,11 +47,11 @@ class StaticPagesController < ApplicationController
         # responseのbody要素をJSON形式で解釈し、hashに変換
         @result = JSON.parse(res.body)
         # 表示用の変数に結果を格納
-        @trackViewUrl ="url:"+@result["results"][0]["trackViewUrl"]
+        #@trackViewUrl ="url:"+@result["results"][0]["trackViewUrl"]
         #for num in 1..9 do
         #  @trackViewUrl[num] =@trackViewUrl+"url:"+@result["results"][num]["trackViewUrl"]
         #end
-        @trackName = @result["results"][0]["trackName"]
+        #@trackName = @result["results"][0]["trackName"]
       # 別のURLに飛ばされた場合
     else
       puts "OMG!! #{res.code} #{res.message}"
