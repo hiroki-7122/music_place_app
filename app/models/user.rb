a1b42bf82a819fe5e2b977b85d19534f624fe044
Hiroki Yano <sxcxyxh@gmail.com>
2017-02-09 01:06:21 +0900

    regist_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  validates :latitude, presence: true
-  validates :longitude, presence: true
-  validates :latitude, uniqueness: true
-  validates :longitude, uniqueness: true
+  #validates :latitude, presence: true
+  #validates :longitude, presence: true
+  #validates :latitude, uniqueness: true
+  #validates :longitude, uniqueness: true
   geocoded_by :address
   after_validation :geocode
 end
